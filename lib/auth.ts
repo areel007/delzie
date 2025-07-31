4247337f5148b2932cb7b4a47f88692a49d8efb1
bamidele <morenikejisunday12@gmail.com>
2025-07-31 13:57:47 +0100

    fix typescript setup issues

---
+++ lib/auth.ts
@@ -0,0 +1,42 @@
+// lib/auth.ts or config/auth.ts
+
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "@/lib/mongodb";
+import User from "@/models/user";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) throw new Error("No credentials provided");
+
+        await connectToDatabase();
+        const user = await User.findOne({ email: credentials.email });
+
+        if (!user) throw new Error("User not found");
+
+        const isPasswordCorrect = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isPasswordCorrect) throw new Error("Wrong password");
+
+        return { id: user._id, email: user.email };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
